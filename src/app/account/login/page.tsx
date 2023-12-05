7a31f5bb30cb754fdf57af0f7ee205bc6af6d853
Marcus-Rise <ilykonst95@gmail.com>
2023-12-05 19:58:06 +0300

    oauth scratch

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,10 +1,10 @@
 import {FC} from "react";
 import YandexWidget from "./yandex-widget";
 
-const Login: FC = async () => {
+const Login: FC = () => {
     const clientId = process.env.YANDEX_CLIENT_ID!;
     const redirectUrl = new URL(process.env.YANDEX_REDIRECT_URL!).href;
-    const tokenPageUrl = new URL( "/api/account/token", redirectUrl).href;
+    const tokenPageUrl = new URL("/api/account/token", redirectUrl).href;
 
     return <>
         <YandexWidget clientId={clientId} redirectUrl={redirectUrl} tokenPageUrl={tokenPageUrl}/>
