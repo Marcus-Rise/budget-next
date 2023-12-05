aaf17b58a7dd195947016cb9974b6ab1c644e9e0
Marcus-Rise <ilykonst95@gmail.com>
2023-12-05 19:39:24 +0300

    oauth scratch

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import YandexWidget from "@/app/account/login/yandex-widget";
 import {headers} from "next/headers";
+import Script from "next/script";
 
 
 const Login: FC = async () => {
@@ -8,6 +9,10 @@ const Login: FC = async () => {
     const redirectUrl = (headers().get('hostname') || "");
 
     return <>
+        <Script
+            strategy={"beforeInteractive"}
+            src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js"
+        />
         <YandexWidget clientId={clientId} redirectUrl={redirectUrl} tokenPageUrl={redirectUrl + "/api/account/token"}/>
     </>;
 }
--- src/app/account/login/yandex-widget.tsx
+++ src/app/account/login/yandex-widget.tsx
@@ -1,25 +1,23 @@
 "use client"
 
-import {FC} from "react";
-import Script from "next/script";
+import {FC, useEffect} from "react";
 
 declare var YaAuthSuggest: any;
 
 const YandexWidget: FC<{ clientId: string; redirectUrl: string; tokenPageUrl: string; }> = ({clientId, redirectUrl, tokenPageUrl}) => {
-    return <Script
-        onLoad={() => {
-            console.debug(YaAuthSuggest)
-            YaAuthSuggest.init(
-                {
-                    client_id: clientId,
-                    response_type: 'token',
-                    redirect_uri: tokenPageUrl
-                },
-                redirectUrl
-            )
-        }}
-        src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js"
-    />
+    useEffect(() => {
+        console.debug(YaAuthSuggest)
+        YaAuthSuggest.init(
+            {
+                client_id: clientId,
+                response_type: 'token',
+                redirect_uri: tokenPageUrl
+            },
+            redirectUrl
+        ).then((data: any) => console.debug(data))
+    }, [clientId, redirectUrl, tokenPageUrl])
+
+    return <></>
 }
 
 export default YandexWidget;
