5b61b208eca19a06535f1bedf3d3222f673f9a01
Marcus-Rise <ilykonst95@gmail.com>
2023-12-06 09:12:54 +0300

    oauth scratch

---
+++ src/app/api/account/token/route.ts
@@ -0,0 +1,14 @@
+import {NextResponse} from "next/server";
+
+const AcceptOauthToken = (req: Request) => {
+    // todo отладить отладочный токен
+    const url = new URL(req.url);
+
+    return NextResponse.json({
+        accessToken: url.searchParams.get("access_token"),
+        tokenType: url.searchParams.get("token_type"),
+        expiresIn: url.searchParams.get("expires_in"),
+    });
+};
+
+export {AcceptOauthToken as GET}
