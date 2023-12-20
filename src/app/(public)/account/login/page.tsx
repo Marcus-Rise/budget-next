71cfe1fcd828298d6dc579edb69131a3b66405cd
Marcus-Rise <ilykonst95@gmail.com>
2023-12-20 13:40:55 +0400

    feat: logo on login screen

--- src/app/(public)/account/login/page.tsx
+++ src/app/(public)/account/login/page.tsx
@@ -2,23 +2,45 @@ import { FC } from 'react';
 import { OauthVkLoginButton } from '@/oauth/vk-login-button';
 import { oauthConfigFactory } from '@/oauth/config';
 import { LoginLink } from '@/app/(public)/account/login/login-link.component';
+import Image from 'next/image';
+import Logo from '@/app/icon.png';
+import Link from 'next/link';
+import metaConfig from '@/meta-config.cjs';
+import { Footer } from '@/app/footer.component';
 
 const Login: FC = () => {
   const { appId, redirectUrl, idApiUrl } = oauthConfigFactory();
 
-  return (
-    <div className={'flex flex-col gap-2'}>
-      <p className={'text-center text-2xl'}>Добро пожаловать!</p>
+  const logoSize = 100;
 
-      <LoginLink
-        className={'mx-auto w-80'}
-        apiUrl={idApiUrl}
-        appId={appId}
-        redirectUri={redirectUrl}
-        responseType={'silent_token'}
-      >
-        <OauthVkLoginButton />
-      </LoginLink>
+  return (
+    <div className={'h-[calc(100dvh)] w-full flex flex-col'}>
+      <main className={'bg-background basis-full self-center'}>
+        <div className={'container flex flex-col gap-7 items-center justify-center h-full'}>
+          <Link href="/">
+            <h1 className={'font-bold text-center text-4xl'}>{metaConfig.title}</h1>
+          </Link>
+
+          <Image alt={'logo'} src={Logo} width={logoSize} height={logoSize} />
+
+          <p className={'text-center text-2xl'}>Добро пожаловать!</p>
+
+          <LoginLink
+            className={'mx-auto w-80'}
+            apiUrl={idApiUrl}
+            appId={appId}
+            redirectUri={redirectUrl}
+            responseType={'silent_token'}
+          >
+            <OauthVkLoginButton />
+          </LoginLink>
+        </div>
+      </main>
+      <Footer
+        authorName={metaConfig.author.name}
+        authorUrl={metaConfig.author.url}
+        className={'bg-background basis-auto'}
+      />
     </div>
   );
 };
