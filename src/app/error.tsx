e0621533b7c21e5d67e180d7ac7b7e00898d9fd9
Marcus-Rise <ilykonst95@gmail.com>
2023-12-25 15:45:20 +0400

    fix: error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 'use client'; // Error components must be Client Components
 
-import { redirect } from 'next/navigation';
 import { Button } from '@/components/button.component';
+import Link from 'next/link';
 
 export default function Error({
   error,
@@ -13,14 +13,9 @@ export default function Error({
   return (
     <div className={'h-screen w-full flex flex-col items-center justify-center gap-3'}>
       <h1 className={'font-medium text-2xl'}>Что-то пошло не так!</h1>
-      <Button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => redirect('/api/account/logout')
-        }
-      >
-        Попробовать войти снова
-      </Button>
+      <Link href={'/api/account/logout'}>
+        <Button>Попробовать войти снова</Button>
+      </Link>
     </div>
   );
 }
