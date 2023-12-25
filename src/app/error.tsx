da2940249513afe529c451429199c4a0a624c9ba
Marcus-Rise <ilykonst95@gmail.com>
2023-12-25 15:41:02 +0400

    fix: error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,7 @@
 'use client'; // Error components must be Client Components
 
-import { JWT_ERROR_MESSAGE } from '@/auth/jwt/jwt.constants';
 import { redirect } from 'next/navigation';
 import { Button } from '@/components/button.component';
-import { useEffect } from 'react';
 
 export default function Error({
   error,
@@ -12,23 +10,16 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    console.log(error.message);
-    if (error.message === JWT_ERROR_MESSAGE) {
-      redirect('/api/account/logout');
-    }
-  }, [error.message]);
-
   return (
     <div className={'h-screen w-full flex flex-col items-center justify-center gap-3'}>
       <h1 className={'font-medium text-2xl'}>Что-то пошло не так!</h1>
       <Button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => redirect('/api/account/logout')
         }
       >
-        Попробовать еще раз
+        Попробовать войти снова
       </Button>
     </div>
   );
