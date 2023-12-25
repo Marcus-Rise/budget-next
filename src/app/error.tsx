a56b23996367464de90b48e6a4d3ebd3ce2f2ed6
Marcus-Rise <ilykonst95@gmail.com>
2023-12-25 15:33:38 +0400

    fix: error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,7 @@
 import { JWT_ERROR_MESSAGE } from '@/auth/jwt/jwt.constants';
 import { redirect } from 'next/navigation';
 import { Button } from '@/components/button.component';
+import { useEffect } from 'react';
 
 export default function Error({
   error,
@@ -11,9 +12,11 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  if (error.message === JWT_ERROR_MESSAGE) {
-    return redirect('/api/account/logout');
-  }
+  useEffect(() => {
+    if (error.message === JWT_ERROR_MESSAGE) {
+      redirect('/api/account/logout');
+    }
+  }, [error.message]);
 
   return (
     <div className={'h-screen w-full flex flex-col items-center justify-center gap-3'}>
