a82a472fcd78e2ae4ff747bbd3117335eab5c43e
Marcus-Rise <ilykonst95@gmail.com>
2023-12-29 18:10:34 +0400

    chore

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,6 @@ import 'server-only';
 import { ConfigFactory } from '@/config/config.interface';
 
 const configFactory: ConfigFactory = () => ({
-  baseUrl: new URL(process.env.BASE_URL!),
   apiBaseUrl: new URL(process.env.VK_API_URL!).href,
   apiVersion: process.env.VK_API_VERSION!,
 });
