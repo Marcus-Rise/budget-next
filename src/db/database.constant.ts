2e70384de40ac1d62e2ad8d67f55952aa39b3149
Marcus-Rise <ilykonst95@gmail.com>
2025-01-10 12:21:27 +0400

    chore: local db

--- src/db/database.constant.ts
+++ src/db/database.constant.ts
@@ -1,4 +1,16 @@
-import { createKysely } from '@vercel/postgres-kysely';
 import type { Database } from '@/db/database.types';
+import { Kysely, PostgresDialect } from 'kysely';
+import pg from 'pg';
+import { createKysely } from '@vercel/postgres-kysely';
 
-export const db = createKysely<Database>();
+export const db = !!process.env.VERCEL_ENV
+  ? createKysely<Database>()
+  : new Kysely<Database>({
+      log: ['query', 'error'],
+      dialect: new PostgresDialect({
+        pool: new pg.Pool({
+          connectionString: process.env.POSTGRES_URL,
+          max: 10,
+        }),
+      }),
+    });
